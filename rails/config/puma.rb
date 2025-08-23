ffd7ff05f2c7de55db044a926f8c377cd6bbdf5c
秋山健 <akiyamatakeru@akiyamakennonotobukkukonpyuta.local>
2025-08-23 14:27:33 +0900

    修正

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -1,7 +1,9 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+# frozen_string_literal: true
+
+threads_count = ENV.fetch('RAILS_MAX_THREADS', 5).to_i
 threads threads_count, threads_count
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch('RAILS_ENV', 'development')
 plugin :tmp_restart
 
-app_root = File.expand_path("..", __dir__)
+app_root = File.expand_path('..', __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
